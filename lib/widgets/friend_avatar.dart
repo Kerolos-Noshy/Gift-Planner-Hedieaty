7286a47aab4fb0a704384dcdcab131b3e0ef21c6
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-10-25 12:41:26 +0300

    turn avatar into a button

--- lib/widgets/friend_avatar.dart
+++ lib/widgets/friend_avatar.dart
@@ -11,20 +11,21 @@ class FriendAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 10),
-      child: Column(
-        children: [
-          Stack(
-            alignment: const AlignmentDirectional(1.2, -1),
-            children: [
-              Container(
+    return Column(
+      children: [
+        Stack(
+          alignment: const AlignmentDirectional(1.2, -1),
+          children: [
+            MaterialButton(
+              onPressed: () {  },
+              padding: EdgeInsets.zero,
+              child: Container(
                 height: avatarSize,
                 width: avatarSize,
                 padding: const EdgeInsets.all(3),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(avatarSize),
-                  border: Border.all(color: Colors.black, width: 1),
+                  border: Border.all(color: const Color(0xFF5AAB37), width: 2),
                   // image: DecorationImage(
                   //   image: AssetImage(friend['profile_image']==""?"assets/images/male.png":friend['profile_image']),
                   //   fit: BoxFit.cover
@@ -32,20 +33,20 @@ class FriendAvatar extends StatelessWidget {
                 ),
                 child: Container(
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(80),
+                      borderRadius: BorderRadius.circular(100),
                       image: DecorationImage(
-                          image: AssetImage(friend['profile_image']==""?"assets/images/user.png":friend['profile_image']),
+                          image: AssetImage(friend['profile_image']==""?(friend['gender']=='m'?"assets/images/male-avatar.png":"assets/images/female-avatar.png"):friend['profile_image']),
                           fit: BoxFit.cover
                       )
                   ),
                 ),
               ),
-              showEventsNotification?const NotificationCircle():const SizedBox(height: 0,width: 0,)
-            ]
-          ),
-          showName?Text(friend['name'].split(' ')[0]):const SizedBox(height: 0,width: 0,)
-        ],
-      ),
+            ),
+            showEventsNotification && friend['upcoming_events_num'] > 0?NotificationCircle(upcomingEventsNum: friend['upcoming_events_num'],):const SizedBox(height: 0,width: 0,)
+          ]
+        ),
+        showName?Text(friend['name'].split(' ')[0]):const SizedBox(height: 0,width: 0,)
+      ],
     );
   }
 }
