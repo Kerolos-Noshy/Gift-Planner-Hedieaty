0dd430931b30738ad136f6ee0ae90ae4481df977
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-10-25 12:42:17 +0300

    update conditions

--- lib/widgets/notification_circle.dart
+++ lib/widgets/notification_circle.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NotificationCircle extends StatelessWidget {
   final int upcomingEventsNum;
-
   const NotificationCircle({super.key, this.upcomingEventsNum=0});
 
   @override
@@ -13,12 +11,12 @@ class NotificationCircle extends StatelessWidget {
       width: 20,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
-          color: upcomingEventsNum > 0 ? Colors.red[600]: Colors.transparent
+          color: Colors.red[600]
       ),
       margin: const EdgeInsets.only(right: 10),
       child: Center(
         child: Text(
-          upcomingEventsNum > 0 ? "$upcomingEventsNum": "",
+          "$upcomingEventsNum",
           style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
