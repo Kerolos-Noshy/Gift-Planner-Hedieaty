de31e0e0b449bcf8b65eede7f79454a365e21666
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-11-17 18:54:15 +0200

    add model

---
+++ lib/models/friend_model.dart
@@ -0,0 +1,23 @@
+class Friend {
+  int userId;
+  int friendId;
+
+  Friend({
+    required this.userId,
+    required this.friendId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'user_id': userId,
+      'friend_id': friendId,
+    };
+  }
+
+  factory Friend.fromMap(Map<String, dynamic> map) {
+    return Friend(
+      userId: map['user_id'],
+      friendId: map['friend_id'],
+    );
+  }
+}
