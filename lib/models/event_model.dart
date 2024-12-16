d558bbfd044d2415db452a27bb8c7476b028ec48
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-16 02:37:34 +0200

    add event model

---
+++ lib/models/event_model.dart
@@ -0,0 +1,40 @@
+class Event {
+  final int? id;
+  final String name;
+  final DateTime date;
+  final String location;
+  final String description;
+  final int userId;
+  // TODO: add event type
+
+  Event({
+    this.id,
+    required this.name,
+    required this.date,
+    required this.location,
+    required this.description,
+    required this.userId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'date': date.toIso8601String(), // Store as a string for SQLite
+      'location': location,
+      'description': description,
+      'user_id': userId,
+    };
+  }
+
+  factory Event.fromMap(Map<String, dynamic> map) {
+    return Event(
+      id: map['id'],
+      name: map['name'],
+      date: DateTime.parse(map['date']),
+      location: map['location'],
+      description: map['description'],
+      userId: map['user_id'],
+    );
+  }
+}
