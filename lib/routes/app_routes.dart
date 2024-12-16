4a020c13f5a89769b86d5ebd66e7961b34ca691c
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-16 02:23:38 +0200

    add routes names

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,5 +1,10 @@
 class AppRoutes{
-  static const homePage = "/";
+  static const homePage = "/home";
   static const allFriends = "/all_friends";
+  static const login = "/";
+  static const signup = "/signup";
+  static const notifications = "/notifications";
+  static const friendProfile = "/friend_profile";
+
 
 }
