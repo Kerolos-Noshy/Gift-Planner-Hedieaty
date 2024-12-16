e5c135afa292e930a7fc54108b991d3800e2d59d
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-16 02:28:03 +0200

    change columns types

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -22,33 +22,37 @@ class DatabaseHelper {
     final path = join(dbPath, 'gifts_app.db');
     return openDatabase(
       path,
-      version: 2,
+      version: 7,
       onCreate: _onCreate,
+      onUpgrade: _onUpgrade
     );
   }
 
-  // SQL for creating tables
   Future _onCreate(Database db, int version) async {
     await db.execute('DROP TABLE IF EXISTS Users');
+    await db.execute('DROP TABLE IF EXISTS Events');
+    await db.execute('DROP TABLE IF EXISTS Gifts');
+    await db.execute('DROP TABLE IF EXISTS Friends');
     await db.execute('''
       CREATE TABLE Users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id TEXT PRIMARY KEY,
         name TEXT NOT NULL,
+        phone TEXT NOT NULL,
         email TEXT NOT NULL,
         preferences TEXT,
         gender TEXT NOT NULL,
-        profile_image_path TEXT
       );
     ''');
 
     await db.execute('''
       CREATE TABLE Events (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firestore_id TEXT,
         name TEXT NOT NULL,
         date TEXT,
         location TEXT,
         description TEXT,
-        user_id INTEGER,
+        user_id TEXT,
         FOREIGN KEY (user_id) REFERENCES Users(id)
       );
     ''');
@@ -56,24 +60,44 @@ class DatabaseHelper {
     await db.execute('''
       CREATE TABLE Gifts (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        firestore_id TEXT,
         name TEXT NOT NULL,
         description TEXT,
         category TEXT,
-        price REAL,
+        price DOUBLE,
         status TEXT,
         event_id INTEGER,
-        FOREIGN KEY (event_id) REFERENCES Events(id)
+        pledger_id TEXT,
+        FOREIGN KEY (event_id) REFERENCES Events(id),
+        FOREIGN KEY (pledger_id) REFERENCES Users(id)
       );
     ''');
 
     await db.execute('''
       CREATE TABLE Friends (
-        user_id INTEGER,
-        friend_id INTEGER,
+        user_id TEXT,
+        friend_id TEXT,
         PRIMARY KEY (user_id, friend_id),
         FOREIGN KEY (user_id) REFERENCES Users(id),
         FOREIGN KEY (friend_id) REFERENCES Users(id)
       );
     ''');
+  }
+  // Handle database upgrades
+  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    if (oldVersion < 7) {
+      // Drop and recreate Users table for schema changes
+      await db.execute('DROP TABLE Users');
+      await db.execute('''
+        CREATE TABLE Users (
+          id TEXT PRIMARY KEY,
+          name TEXT NOT NULL,
+          phone TEXT NOT NULL,
+          email TEXT NOT NULL,
+          preferences TEXT,
+          gender TEXT NOT NULL
+        );
+      ''');
+    }
   }
 }
