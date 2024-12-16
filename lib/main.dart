1d45633a082a5c63d3ad27592cd3a56a4efcb6a0
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-16 02:31:00 +0200

    add fire store init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hedieaty/routes/app_routes.dart';
 import 'package:hedieaty/views/bottom_nav_bar.dart';
 import 'package:hedieaty/views/home/all_friends.dart';
+import 'package:hedieaty/views/login/login_page.dart';
+import 'package:hedieaty/views/profile/notifications_page.dart';
+import 'package:hedieaty/views/signup/signup_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized() ;
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,9 +20,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // home : const LoginPage(),
+
       routes: {
+        AppRoutes.login: (context) => const LoginPage(),
+        AppRoutes.signup: (context) => const SignupPage(),
         AppRoutes.homePage: (context) => const BottomNavBar(),
-        AppRoutes.allFriends: (context) => const AllFriends()
+        AppRoutes.allFriends: (context) => const AllFriends(),
+        AppRoutes.notifications: (context) => const NotificationsPage(),
+        // AppRoutes.
       },
     );
   }
