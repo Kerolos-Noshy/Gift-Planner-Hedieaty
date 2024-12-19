40531cd57ba2aba2e5c92da76fe8c1522b5acaaf
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-19 21:02:38 +0200

    add fire store service functions for friends

--- lib/services/friend_service.dart
+++ lib/services/friend_service.dart
@@ -1,27 +1,102 @@
-import 'package:hedieaty/models/friend_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hedieaty/services/user_service.dart';
 
+import '../models/user_model.dart';
 import 'firestore_service.dart';
 
 class FriendService {
   final FirestoreService _firestoreService = FirestoreService();
+  final _firestore = FirebaseFirestore.instance;
   final String _collectionPath = 'friends';
 
-  Future<void> addFriend(Friend friendData) async {
-    await _firestoreService.addDocument(_collectionPath, friendData.toMap());
-  }
+  Future<void> addFriend(String userId, String friendId) async {
+    try {
+      await _firestore
+        .collection('users')
+        .doc(userId)
+        .collection('friends')
+        .doc(friendId)
+        .set({});
 
-  Future<void> deleteFriend(int userId, int friendId) async {
-    final querySnapshot = await _firestoreService.fetchCollection(_collectionPath);
-    for (var doc in querySnapshot) {
-      if (doc['user_id'] == userId && doc['friend_id'] == friendId) {
-        await _firestoreService.deleteDocument(_collectionPath, doc['id']);
-        break;
-      }
+      await _firestore
+          .collection('users')
+          .doc(friendId)
+          .collection('friends')
+          .doc(userId)
+          .set({});
+      print('Friend added successfully!');
+    } catch (e) {
+      print('Failed to add friend: $e');
     }
   }
 
-  Future<List<Map<String, dynamic>>> fetchFriends(int userId) async {
-    final querySnapshot = await _firestoreService.fetchCollection(_collectionPath);
-    return querySnapshot.where((doc) => doc['user_id'] == userId).toList();
+  Future<List<User>> getFriends(String userId) async {
+    // List<String> friendIds = [];
+    List<User> friends = [];
+
+    try {
+      final snapshot = await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('friends')
+          .get();
+
+      for (var doc in snapshot.docs) {
+        // friendIds.add(doc.id);
+        User? u = await UserService().getUser(doc.id);
+        friends.add(u!);
+      }
+    } catch (e) {
+      print('Failed to get friends: $e');
+    }
+
+    return friends;
+  }
+
+  Future<int> getFriendsCount(String userId) async {
+
+    try {
+      final snapshot = await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('friends')
+          .get();
+
+      return snapshot.size;
+    } catch (e) {
+      print('Failed to get friends: $e');
+    }
+
+    return 0;
+  }
+
+  Future<void> removeFriend(String userId, String friendId) async {
+    try {
+      await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection('friends')
+          .doc(friendId)
+          .delete();
+      print('Friend removed successfully!');
+    } catch (e) {
+      print('Failed to remove friend: $e');
+    }
+  }
+
+  Future<bool> friendshipExists(String userId, String friendId) async {
+    try {
+      final friendDoc = await _firestore
+          .collection(_collectionPath)
+          .doc(userId)
+          .collection('friends')
+          .doc(friendId)
+          .get();
+
+      return friendDoc.exists;
+    } catch (e) {
+      print('Failed to check friendship: $e');
+      return false;
+    }
   }
 }
