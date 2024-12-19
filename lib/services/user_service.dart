efd1d63c6b8f9146fa087dc016c971eda7ade46d
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-19 21:00:48 +0200

    add firestore functions for user

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,24 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hedieaty/models/user_model.dart';
 
-import 'firestore_service.dart';
 
 class UserService {
-  final FirestoreService _firestoreService = FirestoreService();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final String _collectionPath = 'users';
 
   Future<void> addUser(User userData) async {
-    await _firestoreService.addDocument(_collectionPath, userData.toMap());
+    try {
+      await _firestore.collection(_collectionPath).doc(userData.id).set(userData.toMap());
+      print('User added successfully');
+    } catch (e) {
+      print('Failed to add user: $e');
+    }
   }
 
-  Future<void> updateUser(String id, User userData) async {
-    await _firestoreService.updateDocument(_collectionPath, id, userData.toMap());
+  // Update user details
+  Future<void> updateUser(String userId, User userData) async {
+    try {
+      await _firestore.collection(_collectionPath).doc(userId).update(userData.toMap());
+      print('User updated successfully');
+    } catch (e) {
+      print('Failed to update user: $e');
+    }
   }
 
-  Future<void> deleteUser(String id) async {
-    await _firestoreService.deleteDocument(_collectionPath, id);
+  // Fetch user details
+  Future<User?> getUser(String userId) async {
+    try {
+      final doc = await _firestore.collection(_collectionPath).doc(userId).get();
+      if (doc.exists) {
+        return User.fromMap(doc.data()!);
+      }
+      print('User not found');
+      return null;
+    } catch (e) {
+      print('Failed to fetch user: $e');
+      return null;
+    }
   }
 
-  Future<List<Map<String, dynamic>>> fetchUsers() async {
-    return await _firestoreService.fetchCollection(_collectionPath);
+  Future<bool> doesPhoneNumberExist(String phoneNumber) async {
+    try {
+      final snapshot = await _firestore
+          .collection(_collectionPath)
+          .where('phone', isEqualTo: phoneNumber)
+          .get();
+      for (var doc in snapshot.docs)
+        print(doc.data());
+
+      return snapshot.docs.isNotEmpty;
+    } catch (e) {
+      print('Failed to check phone number: $e');
+      return false;
+    }
+  }
+
+  Future<User?> getUserByPhone(String phoneNumber) async {
+    try {
+      final snapshot = await _firestore
+          .collection(_collectionPath)
+          .where('phone', isEqualTo: phoneNumber)
+          .get();
+
+      if (snapshot.docs.isNotEmpty) {
+        return User.fromMap(snapshot.docs.first.data());
+      }
+      print('User with phone number $phoneNumber not found');
+      return null;
+    } catch (e) {
+      print('Failed to get user by phone: $e');
+      return null;
+    }
   }
 }
