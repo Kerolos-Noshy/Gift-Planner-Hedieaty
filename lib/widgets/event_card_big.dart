b9f967e3a4c83b3cb2acc4555aba3ba7f0ac89f9
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-19 04:11:26 +0200

    add event time and visibility

--- lib/widgets/event_card_big.dart
+++ lib/widgets/event_card_big.dart
@@ -3,179 +3,177 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hedieaty/constants/styles/app_styles.dart';
 import 'package:hedieaty/views/event/event_card_field_small.dart';
 import 'package:hedieaty/views/event/event_details.dart';
-import 'dart:math' as math;
+
+import '../models/event_model.dart';
+import '../models/user_model.dart';
+import '../utils/utils.dart';
 
 
-class EventCardBig extends StatelessWidget {
-  // final Event event;
-  const EventCardBig({super.key});
+class EventCardBig extends StatefulWidget {
+  final Event event;
+  final User eventCreator;
+  const EventCardBig({
+    super.key,
+    required this.event,
+    required this.eventCreator
+  });
+
+
+  @override
+  State<EventCardBig> createState() => _EventCardBigState();
+}
+
+class _EventCardBigState extends State<EventCardBig> {
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Stack(
-        alignment: const AlignmentDirectional(-1, -0.75),
+        alignment: AlignmentDirectional.topCenter,
         children: [
-          Stack(
-            alignment: AlignmentDirectional.topCenter,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 35, bottom: 30),
-                padding: const EdgeInsets.only(right: 20, left: 20, top: 45, bottom: 5),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      spreadRadius: 0.2,
-                      blurRadius: 3,
-                      offset: const Offset(0, 1.5),
-                    ),
-                  ],
+          Container(
+            margin: const EdgeInsets.only(top: 35, bottom: 30),
+            padding: const EdgeInsets.only(right: 15, left: 20, top: 45, bottom: 5),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 0.3,
+                  blurRadius: 3,
+                  offset: const Offset(0, 1.5),
                 ),
-                child: Column(
+              ],
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                            "Leo Messi",
-                            style: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600))
-                        ),
-                      ],
+                    Text(
+                      widget.eventCreator.name,
+                      style: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600))
                     ),
-                    const SizedBox(height: 15,),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            EventCardFieldSmall(
-                                ico: Icons.celebration_rounded,
-                                text: "Birthday Party"
-                            ),
-                            // Row(
-                            //   children: [
-                            //     Icon(Icons.cake, color: Colors.grey[800]),
-                            //     const SizedBox(width: 5,),
-                            //     Text(
-                            //         "Birthday Party",
-                            //         style: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600))
-                            //     ),
-                            //   ],
-                            // ),
-                            EventCardFieldSmall(
-                                ico: Icons.card_giftcard_rounded,
-                                text: "Remaining Gifts: 4"
-                            ),
-                          ],
-                        ),
-                        SizedBox(width: 10,),
-                        Column(
-                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            EventCardFieldSmall(
-                                ico: Icons.calendar_month,
-                                text: "30-12-2024"
-                            ),
-                            // Row(
-                            //   children: [
-                            //     Icon(Icons.calendar_month, color: Colors.grey[800]),
-                            //     const SizedBox(width: 5,),
-                            //     Text(
-                            //         "30-12-2024",
-                            //         style: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600))
-                            //     ),
-                            //   ],
-                            // ),
-                            EventCardFieldSmall(
-                                ico: Icons.timer_outlined,
-                                text: "After 34 days",
-                            ),
-                            // Text(
-                            //     "After 34 days",
-                            //     style: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600))
-                            // ),
-                          ],
-                        ),
-                      ],
-                    ),
-
-                    MaterialButton(
-                      child: Text(
-                        "See Details",
-                        style: GoogleFonts.poppins(
-                          textStyle: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.blueAccent
-                          )
-                        ),
-                      ),
-                      onPressed: () {
-                        showModalBottomSheet(
-                          backgroundColor: const Color(0xFFebebeb),
-                          showDragHandle: true,
-                          useSafeArea: true,
-                          isScrollControlled: true,
-                          context: context,
-                          builder: (BuildContext context) {
-                            return const EventDetails();
-                          },
-                        );
-                      },
-                    )
                   ],
                 ),
-              ),
-
-              Container(
-                padding: const EdgeInsets.all(1),
-                decoration: BoxDecoration(
-                  border: Border.symmetric(
-                    vertical: BorderSide(width: 3, color: AppStyles.bgColor),
-                    horizontal:  BorderSide(width: 3, color: AppStyles.bgColor),
-                  ),
-                  borderRadius: BorderRadius.circular(100),
-
-                ),
-                child: Container(
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    image: const DecorationImage(
-                      image: AssetImage("assets/images/male-avatar.png"), fit: BoxFit.cover,
+                const SizedBox(height: 15,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        EventCardFieldSmall(
+                            ico: Icons.celebration_rounded,
+                            text: widget.event.eventType
+                        ),
+                        // TODO: create a function that return the number of the remaining gifts of event
+                        const EventCardFieldSmall(
+                            ico: Icons.card_giftcard_rounded,
+                            text: "Remaining Gifts: 4"
+                        ),
+                        // TODO: add a condition for event status (private or public)
+                        // widget.event.status == 1 ?
+                        const EventCardFieldSmall(
+                          ico: Icons.visibility_off_outlined,
+                          text: "Private Event",
+                          icon_bg_color: Colors.red,
+                        )
+                        // : const EventCardFieldSmall(
+                        //   ico: Icons.visibility_outlined,
+                        //   text: "Public Event",
+                        //   // icon_bg_color: Colors.green,
+                        // ),
+                      ],
                     ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.6),
-                        spreadRadius: 0.2,
-                        blurRadius: 2,
-                        offset: const Offset(0, 0),
-                      ),
-                    ],
-                  ),
+                    const SizedBox(width: 10,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        EventCardFieldSmall(
+                            ico: Icons.calendar_month,
+                            text: formatDate(widget.event.date)
+                        ),
+                        // TODO: add a function that calculate the difference between now and event day
+                        EventCardFieldSmall(
+                            ico: Icons.access_time_outlined,
+                            text: TimeOfDay(
+                                hour:widget.event.date.hour,
+                                minute: widget.event.date.minute
+                            ).format(context),
+                        ),
+                        EventCardFieldSmall(
+                          ico: Icons.timer_outlined,
+                          text: "After ${calculateDaysDifference(widget.event.date).toString()} days",
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 10,),
+                  ],
                 ),
-              ),
-            ]
+
+                MaterialButton(
+                  child: Text(
+                    "See Details",
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.blueAccent
+                      )
+                    ),
+                  ),
+                  onPressed: () {
+                    showModalBottomSheet(
+                      backgroundColor: const Color(0xFFebebeb),
+                      showDragHandle: true,
+                      useSafeArea: true,
+                      isScrollControlled: true,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return EventDetails(eventData: widget.event, eventCreator: widget.eventCreator);
+                      },
+                    );
+                  },
+                )
+              ],
+            ),
           ),
-          Transform.rotate(
-              angle: - math.pi / 4,
-            child:
-            Container(
-              width: 50,
-              height: 50,
-              decoration: const BoxDecoration(
-                // borderRadius: BorderRadius.circular(100),
-                // image: DecorationImage(
-                //   image: AssetImage("assets/images/bow.png"), fit: BoxFit.cover,
-                // ),
+
+          Container(
+            padding: const EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              border: Border.symmetric(
+                vertical: BorderSide(width: 3, color: AppStyles.bgColor),
+                horizontal:  BorderSide(width: 3, color: AppStyles.bgColor),
+              ),
+              borderRadius: BorderRadius.circular(100),
+
+            ),
+            child: Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                image: const DecorationImage(
+                  image: AssetImage("assets/images/male-avatar.png"), fit: BoxFit.cover,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.6),
+                    spreadRadius: 0.2,
+                    blurRadius: 2,
+                    offset: const Offset(0, 0),
+                  ),
+                ],
               ),
             ),
           ),
