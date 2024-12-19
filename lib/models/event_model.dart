78116cb6a4a98b21f2f12f4ccce1e7bff8b5bf3a
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-19 21:01:42 +0200

    add documentId and isPublic

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -1,15 +1,17 @@
 class Event {
-  final int? id;
+  final int? id; // Auto-incremented ID from the local database
+  late String? documentId = null; // Firestore document ID
   final String name;
   final DateTime date;
   final String location;
   final String description;
   final String userId;
   final String eventType;
-  final bool isPublic;
+  late bool isPublic = false;
 
   Event({
     this.id,
+    this.documentId,
     required this.name,
     required this.date,
     required this.location,
@@ -22,19 +24,21 @@ class Event {
   Map<String, dynamic> toMap() {
     return {
       'id': id,
+      'document_id': documentId,
       'name': name,
-      'date': date.toIso8601String(), // Store as a string for SQLite
+      'date': date.toIso8601String(),
       'location': location,
       'description': description,
       'user_id': userId,
       'event_type': eventType,
-      "is_public": isPublic ? 1 : 0,
+      'is_public': isPublic ? 1 : 0,
     };
   }
 
   factory Event.fromMap(Map<String, dynamic> map) {
     return Event(
       id: map['id'],
+      documentId: map['document_id'],
       name: map['name'],
       date: DateTime.parse(map['date']),
       location: map['location'],
