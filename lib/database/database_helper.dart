23e55c7a5c21229885b2e21c5a1a2c81583c314a
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-20 19:37:36 +0200

    add reset database function

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -22,9 +22,8 @@ class DatabaseHelper {
     final path = join(dbPath, 'gifts_app.db');
     return openDatabase(
       path,
-      version: 7,
+      version: 9,
       onCreate: _onCreate,
-      onUpgrade: _onUpgrade
     );
   }
 
@@ -40,19 +39,21 @@ class DatabaseHelper {
         phone TEXT NOT NULL,
         email TEXT NOT NULL,
         preferences TEXT,
-        gender TEXT NOT NULL,
+        gender TEXT NOT NULL
       );
     ''');
 
     await db.execute('''
       CREATE TABLE Events (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        firestore_id TEXT,
+        document_id TEXT,
         name TEXT NOT NULL,
         date TEXT,
         location TEXT,
         description TEXT,
         user_id TEXT,
+        event_type Text,
+        is_public INTEGER,
         FOREIGN KEY (user_id) REFERENCES Users(id)
       );
     ''');
@@ -60,13 +61,15 @@ class DatabaseHelper {
     await db.execute('''
       CREATE TABLE Gifts (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        firestore_id TEXT,
+        document_id TEXT,
         name TEXT NOT NULL,
         description TEXT,
         category TEXT,
         price DOUBLE,
         status TEXT,
         event_id INTEGER,
+        event_doc_id TEXT,
+        gift_creator_id TEXT,
         pledger_id TEXT,
         FOREIGN KEY (event_id) REFERENCES Events(id),
         FOREIGN KEY (pledger_id) REFERENCES Users(id)
@@ -83,21 +86,66 @@ class DatabaseHelper {
       );
     ''');
   }
-  // Handle database upgrades
-  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
-    if (oldVersion < 7) {
-      // Drop and recreate Users table for schema changes
-      await db.execute('DROP TABLE Users');
-      await db.execute('''
-        CREATE TABLE Users (
-          id TEXT PRIMARY KEY,
-          name TEXT NOT NULL,
-          phone TEXT NOT NULL,
-          email TEXT NOT NULL,
-          preferences TEXT,
-          gender TEXT NOT NULL
-        );
-      ''');
-    }
+
+  Future<void> resetDatabase() async {
+    final db = await database;
+    // await db.execute('DROP TABLE IF EXISTS Users');
+    await db.execute('DROP TABLE IF EXISTS Events');
+    await db.execute('DROP TABLE IF EXISTS Gifts');
+    // await db.execute('DROP TABLE IF EXISTS Friends');
+
+    // await db.execute('''
+    //   CREATE TABLE Users (
+    //     id TEXT PRIMARY KEY,
+    //     name TEXT NOT NULL,
+    //     phone TEXT NOT NULL,
+    //     email TEXT NOT NULL,
+    //     preferences TEXT,
+    //     gender TEXT NOT NULL
+    //   );
+    // ''');
+
+    await db.execute('''
+      CREATE TABLE Events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        document_id TEXT,
+        name TEXT NOT NULL,
+        date TEXT,
+        location TEXT,
+        description TEXT,
+        user_id TEXT,
+        event_type Text,
+        is_public INTEGER,
+        FOREIGN KEY (user_id) REFERENCES Users(id)
+      );
+    ''');
+
+    await db.execute('''
+      CREATE TABLE Gifts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        document_id TEXT,
+        name TEXT NOT NULL,
+        description TEXT,
+        category TEXT,
+        price DOUBLE,
+        status TEXT,
+        event_id INTEGER,
+        event_doc_id TEXT,
+        gift_creator_id TEXT,
+        pledger_id TEXT,
+        FOREIGN KEY (event_id) REFERENCES Events(id),
+        FOREIGN KEY (pledger_id) REFERENCES Users(id)
+      );
+    ''');
+    //
+    // await db.execute('''
+    //   CREATE TABLE Friends (
+    //     user_id TEXT,
+    //     friend_id TEXT,
+    //     PRIMARY KEY (user_id, friend_id),
+    //     FOREIGN KEY (user_id) REFERENCES Users(id),
+    //     FOREIGN KEY (friend_id) REFERENCES Users(id)
+    //   );
+    // ''');
   }
 }
