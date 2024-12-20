53338ffb8865f00ba2731252e74a9b3b9ca52616
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-20 09:30:26 +0200

    update gift model

--- lib/models/gift_model.dart
+++ lib/models/gift_model.dart
@@ -1,45 +1,59 @@
 class Gift {
   int? id;
+  String? documentId;
   String name;
   String? description;
-  String? category;
-  double? price;
-  String? status;
-  int? eventId;
+  String category;
+  double price;
+  String status;
+  int eventId;
+  String eventDocId; // firestore event id
+  String giftCreatorId;
+  late String? pledgerId = null;
+
 
   Gift({
     this.id,
+    this.documentId,
     required this.name,
-    this.description,
-    this.category,
-    this.price,
-    this.status,
-    this.eventId,
+    required this.description,
+    required this.category,
+    required this.price,
+    required this.status,
+    required this.eventId,
+    required this.giftCreatorId,
+    required this.eventDocId,
+    this.pledgerId
   });
 
-  // Convert Gift object to a Map (for SQLite insertion)
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
+      'document_id': documentId,
       'name': name,
       'description': description,
       'category': category,
       'price': price,
       'status': status,
       'event_id': eventId,
+      'event_doc_id': eventDocId,
+      'gift_creator_id': giftCreatorId,
+      'pledger_id': pledgerId
     };
   }
 
-  // Create a Gift object from a Map (retrieved from SQLite or Firestore)
   factory Gift.fromMap(Map<String, dynamic> map) {
     return Gift(
       id: map['id'],
+      documentId: map['document_id'],
       name: map['name'],
       description: map['description'],
       category: map['category'],
       price: map['price']?.toDouble(),
       status: map['status'],
       eventId: map['event_id'],
+      eventDocId: map['event_doc_id'],
+      giftCreatorId: map['gift_creator_id'],
+      pledgerId: map['pledger_id']
     );
   }
 }
