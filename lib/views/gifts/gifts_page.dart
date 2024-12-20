faaf5036655556ad46e5da9d632f51e6ec7b6341
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2024-12-20 09:31:06 +0200

    add my pledged gifts page

--- lib/views/gifts/gifts_page.dart
+++ lib/views/gifts/gifts_page.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hedieaty/services/auth_service.dart';
+import 'package:hedieaty/services/event_service.dart';
+import 'package:hedieaty/views/gifts/gift_item.dart';
 
-class GiftsPage extends StatelessWidget {
+import '../../models/event_model.dart';
+import '../../models/gift_model.dart';
+import '../../services/gift_service.dart';
+
+class GiftsPage extends StatefulWidget {
   const GiftsPage({super.key});
+
+  @override
+  State<GiftsPage> createState() => _GiftsPageState();
+}
+
+class _GiftsPageState extends State<GiftsPage> {
+  Future<List<Map<String, dynamic>>> fetchPledgedGiftsData() async {
+    final gifts = await GiftService.getPledgedGifts(AuthService().getCurrentUser().uid);
+
+    return Future.wait(gifts.map((gift) async {
+      final event = await EventService().getEventById(gift.giftCreatorId, gift.eventDocId);
+
+      return {
+        'gift': gift,
+        'event': event,
+      };
+    }).toList());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +37,7 @@ class GiftsPage extends StatelessWidget {
         surfaceTintColor: const Color(0xFFf5f4f3),
         shadowColor: Colors.grey,
         title: Text(
-            "Gifts",
+            "My Pledged Gifts",
             style: GoogleFonts.markaziText(
                 textStyle: const TextStyle(
                     fontSize: 35,
@@ -23,11 +48,47 @@ class GiftsPage extends StatelessWidget {
       ),
       backgroundColor: const Color(0xFFf5f4f3),
       body: SafeArea(
-          child: Column(
-            children: [
-              const SizedBox(height: 15,),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+                const SizedBox(height: 15,),
+
+
+              FutureBuilder<List<Map<String, dynamic>>>(
+                future: fetchPledgedGiftsData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No pledged gifts found.'));
+                  } else {
+                    final pledgedGiftsData = snapshot.data!;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: pledgedGiftsData.length,
+                      itemBuilder: (context, index) {
+                        final data = pledgedGiftsData[index];
+                        final gift = data['gift'] as Gift;
+                        final event = data['event'] as Event;
+
+                        return GiftItem(
+                          gift: gift,
+                          event: event,
+                          onDeleteGift: () {},
+                          showPledgedGiftDetails: true,
+                          // bgColor: Color(0x83CFCAC6),
+                        );
+                      },
+                    );
+                  }
+                },
+              )
             ],
-          )
+          ),
+        )
       ),
     );
   }
